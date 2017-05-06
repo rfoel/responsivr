3b2a21aab5fb9912f311131e78be7dda23c16fc3
Rafael Franco <rafaelfr@outlook.com>
2017-05-05 22:03:36 -0300

    Major update

--- dev/js/init.js
+++
@@ -1,5 +0,0 @@
-$('.scrollspy').scrollSpy();
-$('#select1').selecty();
-$('#select2').selecty();
-$('#select3').selecty();
-$('#select4').selecty();
--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,37 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var sass = require('gulp-ruby-sass');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var uglifycss = require('gulp-uglifycss');
-var imagemin = require('gulp-imagemin');
-var browserSync = require('browser-sync').create();
+'use strict';
 
-gulp.task('default', ['sass', 'js', 'js-init', 'views', 'watch', 'server']);
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pkg = require('./package.json');
+var minifyCSS = require('gulp-minify-css');
+var comment = '\/*\r\n* Responsivr ' + pkg.version + '\r\n* Copyright 2017, Rafael Franco\r\n* https:\/\/rfoel.github.io\/responsivr\/\r\n* Free to use under the MIT license.\r\n *\/\r\n';
+var $ = require('gulp-load-plugins')();
+
+gulp.task('build', function () {
+  return gulp.src('./src/responsivr.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe($.concat('responsivr.css'))
+    .pipe($.header(comment + '\n'))
+    .pipe($.size())
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minify', ['build'], function() {
+  return gulp.src(['./dist/responsivr.css'])
+    .pipe(minifyCSS())
+    .pipe($.header(comment))
+    .pipe($.size())
+    .pipe($.size({
+      gzip: true
+    }))
+    .pipe($.concat('responsivr.min.css'))
+    .pipe(gulp.dest('./dist/'));
+});
+
 
 gulp.task('watch', function() {
-	gulp.watch('./dev/sass/**/*', ['sass']);
-	gulp.watch('./dev/js/**/*', ['js']);
-	gulp.watch('./dev/views/**/*', ['views']);
+  gulp.watch(['src/*.scss'], ['default']);
 });
 
-gulp.task('fonts', function() {
-	return gulp.src(['./dev/fonts/**'])
-	.pipe(gulp.dest('./dist/fonts/'));
-});
 
-gulp.task('sass', function() {
-	sass('./dev/sass/responsivr.scss')
-	.on('error', sass.logError)
-	// .pipe(uglifycss())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('./dist/css/'));
-});
-
-gulp.task('views', function () {
-	return gulp.src('./dev/views/*.pug')
-	.pipe(pug({
-		pretty: true
-	}))
-	.pipe(gulp.dest('./pages/'));
-});
-
-gulp.task('jslib', function() {
-	return gulp.src(['./node_modules/jquery/dist/jquery.js', './dev/js/prism.js'])
-	.pipe(concat('lib.js'))
-	// .pipe(uglify())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('./dist/js/'));
-});
-
-gulp.task('css', function() {
-	return gulp.src(['./dev/css/*'])
-	.pipe(concat('page.css'))
-	// .pipe(uglify())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('./dist/css/'));
-});
-
-gulp.task('js', function() {
-	return gulp.src([
-		'./node_modules/jquery.easing/jquery.easing.js',
-		'./dev/js/scrollspy.js', 
-		'./dev/js/waves.js',
-		'./dev/js/selecty.js',
-		'./dev/js/responsivr.js'
-		])
-	.pipe(concat('responsivr.js'))
-	// .pipe(uglify())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('./dist/js/'));
-});
-
-gulp.task('js-init', function() {
-	return gulp.src([
-		'./dev/js/init.js', 
-		])
-	// .pipe(uglify())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('./dist/js/'));
-});
-
-gulp.task('images', function() {
-	gulp.src('./dev/images/*')
-	.pipe(imagemin())
-	.pipe(gulp.dest('./dist/images/'));
-});
-
-gulp.task('server', function() {
-  browserSync.init({
-    files: ['./dist/**','./*.html'],
-    port: '8888',
-    server: {
-      baseDir: ['dist', 'pages']
-    }
-  })
-});
+gulp.task('default', ['build', 'minify']);
